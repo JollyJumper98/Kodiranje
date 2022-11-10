d41cd12f1d3283b99316df6d2a4d0fb712cf1df2
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-10 20:42:39 +0100

    Init

---
+++ SelenijumViprTest/SelenijumViprTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelenijumViprTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            element.SendKeys("Selenium");
+            
+
+            IWebElement elementButton = driver.FindElement(By.Name("btnK"));
+            elementButton.Click();
+
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumDrugiTest/SeleniumDrugiTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDrugiTest
+{
+    public class Program
+    {
+        IWebDriver driver = new ChromeDriver(); 
+        static void Main(string[] args)
+        {
+          
+
+        }
+        [SetUp]
+        public void Inicijalizacija()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Window.Maximize();
+
+        }
+        [Test]
+        public void Test1()
+        {
+            IWebElement username = driver.FindElement(By.Id("user-name"));
+            username.SendKeys("standard_user");
+
+            IWebElement password = driver.FindElement(By.Id("password"));
+            password.SendKeys("secret_sauce");
+
+            IWebElement loginButton = driver.FindElement(By.Id("login-button"));
+            loginButton.Submit();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
